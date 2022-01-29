00e9c5cf9867fef00a57ff1a4b9f3a0667e13489
Sebastian Manriquez <seba.manriquezh@gmail.com>
2022-01-29 12:26:14 -0300

    paquetes nuget added, startup file and settings

---
+++ WebApiCampaign/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiCampaign
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
